cdf177314c9b6eeef41b8865e71ed43acf0d1372
Александр Сминов ПрИ-202 <smirnovalexandr234@gmail.com>
2025-02-16 19:06:12 +0500

    Default Bootstrap

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,16 +1,27 @@
 package com.example.demo.model;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Post {
     private String text;
     private int likes;
+    private Date creationDate;
 
     public Post(String text){
         this.text = text;
         this.likes = 0;
+        this.creationDate = new Date();
+    }
+    public Post(String text, Date creationDate){
+        this.text = text;
+        this.likes = 0;
+        this.creationDate = creationDate;
     }
 
     public String getText() {return text;}
     public int getLikes() {return likes;}
+    public Date getCreationDate() {return creationDate;}
 
     public void Like(){likes++;}
 }
