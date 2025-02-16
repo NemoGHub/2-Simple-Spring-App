daa70efd0046e1b326657aeb91743c00429fcc09
Александр Сминов ПрИ-202 <smirnovalexandr234@gmail.com>
2025-02-16 17:26:14 +0500

    Thymeleaf

--- src/main/java/com/example/demo/PostsViewController.java
+++ src/main/java/com/example/demo/PostsViewController.java
@@ -1,17 +1,19 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
+//    @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё Спринг-приложение");
+        return "list";
     }
 
-    @ResponseBody
+//    @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
